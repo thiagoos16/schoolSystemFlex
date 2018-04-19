fab5d42aa578806124cc7f562863b78e24c5beb7
thiagometal <thiagometal.fne@gmail.com>
2018-04-19 14:53:28 -0400

    adiciona migration da tabela lista_alunos

---
+++ database/migrations/2018_04_19_184412_create_lista_alunos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListaAlunosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lista_alunos', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('id_aluno')->unsigned();
+            $table->foreign('id_aluno')->references('id')->on('aluno');
+
+            $table->integer('id_turma')->unsigned();
+            $table->foreign('id_turma')->references('id')->on('turma');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table::table('lista_alunos', function(Blueprint $table){
+            $table->dropForeign('id_aluno');
+        });
+
+        $table::table('lista_alunos', function(Blueprint $table){
+            $table->dropForeign('id_turma');
+        });
+
+        Schema::dropIfExists('lista_alunos');
+    }
+}
