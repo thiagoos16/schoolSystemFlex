4e39a95bdfc4ad40f569b4cc41f95cebb9701b62
thiagometal <thiagometal.fne@gmail.com>
2018-04-19 16:41:27 -0400

    adiciona model de disciplina

---
+++ app/Disciplina.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Disciplina extends Model
+{
+    protected $table = 'disciplina';
+
+    protected $fillable = ['Nome'];
+}
